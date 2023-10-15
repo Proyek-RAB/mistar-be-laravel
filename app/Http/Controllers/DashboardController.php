a703560a82b246b1cf44179d085ca2df35f431d4
akramab <13519142@std.stei.itb.ac.id>
2023-10-15 15:59:18 +0700

    feat: update api

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,38 +9,6 @@ class DashboardController extends Controller
     public function dashboard() {
         return response()->json(
             [
-                'sucess' => true,
-                'message' => 'success',
-                'data' => [
-                    'infrastruktur' => [
-                        'status' => [
-                            'baik' => 3,
-                            'perbaikan' => 1,
-                            'rusak' => 2
-                        ],
-                        'total' => 6,
-                        'titik'=> [
-                            'label' => ['Air Bersih', 'Air Limbah (Jamban)', 'Air Sumur'],
-                            'count' => [1, 2, 0]
-                        ],
-                        'garis' => [
-                            'label' => ['Drainase', 'Jalan'],
-                            'count' => [0, 1]
-                        ],
-                        'bidang' => [
-                            'label' => ['Lahan Parkir'],
-                            'count' => [2]
-                        ],
-                        'report' => [
-                            'month' => ['Juni', 'Juli', 'Agustus', 'September'],
-                            'count' => [
-                                'titik' => [0,0,0,3],
-                                'garis' => [0,0,0,1],
-                                'bidang' => [0,0,0,2]
-                            ]
-                        ]
-                    ]
-                ]
 
             ]
         );
@@ -49,42 +17,7 @@ class DashboardController extends Controller
     public function point() {
         return response()->json(
             [
-                'success' => true,
-                'message' => 'success get point',
-                'data' => [
-                    'infrastruktur' => [
-                        [
-                            'id' => 1,
-                            'nama' => 'Mata Air Sumur Ayu',
-                            'tipe' => 'Air Bersih',
-                            'kepemilikan' => 'Publik',
-                            'created_by' => 'akram',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ],
-                        [
-                            'id' => 2,
-                            'nama' => 'Pembuangan Akhir WC RT 5',
-                            'tipe' => 'Air Kotor (Jamban)',
-                            'kepemilikan' => 'Publik',
-                            'created_by' => 'akram',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ],
-                        [
-                            'id' => 3,
-                            'nama' => 'Jamban Bersama',
-                            'tipe' => 'Air Kotor (Jamban)',
-                            'kepemilikan' => 'Publik',
-                            'created_by' => 'akram',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ]
-                    ],
-                    'page' => 1,
-                    'total_page' => 1,
-                    'total_data' => 3,
-                ]
+
             ]
         );
     }
@@ -92,24 +25,7 @@ class DashboardController extends Controller
     public function line() {
         return response()->json(
             [
-                'success' => true,
-                'message' => 'success get line',
-                'data' => [
-                    'infrastruktur' => [
-                        [
-                            'id' => 1,
-                            'nama' => 'Jalan Rakyat',
-                            'tipe' => 'Jalan',
-                            'tingkatan' => 'Umum',
-                            'created_by' => 'akram',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ]
-                    ],
-                    'page' => 1,
-                    'total_page' => 1,
-                    'total_data' => 1,
-                ]
+
             ]
         );
     }
@@ -117,33 +33,7 @@ class DashboardController extends Controller
     public function area() {
         return response()->json(
             [
-                'success' => true,
-                'message' => 'success get area',
-                'data' => [
-                    'infrastruktur' => [
-                        [
-                            'id' => 1,
-                            'nama' => 'Tempat Parkir RT 5',
-                            'tipe' => 'Lahan Parkir',
-                            'kepemilikan' => 'Publik',
-                            'created_by' => 'akram',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ],
-                        [
-                            'id' => 2,
-                            'nama' => 'Tempar Parkir Liar',
-                            'tipe' => 'Lahan Parkir',
-                            'kepemilikan' => 'Publik',
-                            'created_by' => 'akram',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ]
-                    ],
-                    'page' => 1,
-                    'total_page' => 1,
-                    'total_data' => 2,
-                ]
+
             ]
         );
     }
@@ -151,23 +41,7 @@ class DashboardController extends Controller
     public function user() {
         return response()->json(
             [
-                'success' => true,
-                'message' => 'success get user',
-                'data' => [
-                    'users' => [
-                        [
-                            'id' => 1,
-                            'nama' => 'akram',
-                            'email' => 'akram@mail.com',
-                            'role' => 'USER',
-                            'created_at' => ' 2023-09-30 16:20:00.00',
-                            'updated_at' => ' 2023-09-30 16:20:00.00',
-                        ]
-                    ],
-                    'page' => 1,
-                    'total_page' => 1,
-                    'total_data' => 1,
-                ]
+
             ]
         );
     }
--- app/Models/Infrastructure.php
+++ app/Models/Infrastructure.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Infrastructure extends Model
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
