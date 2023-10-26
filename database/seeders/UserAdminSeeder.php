d257496b37d8851d95dfb8a6e8bf3f8dbd908e5e
akramab <13519142@std.stei.itb.ac.id>
2023-10-26 19:04:47 +0700

    feat: update

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'full_name' => 'admin jago',
+            'email' => 'admin@gmail.com',
+            'role' => User::ROLE_ADMIN,
+            'password' => 'admin',
+            'avatar_url' => 'https://www.clipartmax.com/png/middle/347-3473462_blue-icon-data-public-clip-art-black-and-white-library-link-icon.png',
+        ]);
+    }
+}
