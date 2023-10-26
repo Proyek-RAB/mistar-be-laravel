05ddc336bc3aee907aea084b7f96663b0e9e385a
akramab <13519142@std.stei.itb.ac.id>
2023-10-26 18:20:27 +0700

    feat: update seeder

---
+++ app/Http/Resources/InfrastructureResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InfrastructureResource extends JsonResource
+{
+    public static $wrap = 'infrastruktur';
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type' => $this->type,
+            'sub_type' => $this->sub_type,
+            'status' => $this->status,
+            'approved_status' => $this->approved_status,
+            'detail' => json_decode($this->detail),
+            'created_by' => $this->user->full_name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
--- app/Models/Infrastructure.php
+++ app/Models/Infrastructure.php
@@ -14,6 +14,31 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Infrastructure extends Model
 {
     use HasFactory, HasUuids;
+
+    const TYPE_POINT = 'Titik';
+
+    const TYPE_LINE = 'Garis';
+
+    const TYPE_AREA = 'Bidang';
+
+    const SUB_TYPE_CLEAN_WATER = 'Air Bersih';
+
+    const SUB_TYPE_DIRTY_WATER = 'Air Kotor (Jamban)';
+
+    const SUB_TYPE_WASTE = 'Persampahan';
+
+    const SUB_TYPE_ROAD_DRAINAGE = 'Jalan & Drainase';
+
+    const SUB_TYPE_PARKING_LOT = 'Lahan Parkir';
+
+    const STATUS_GOOD = 'baik';
+
+    const STATUS_REPAIR = 'perbaikan';
+
+    const STATUS_BROKEN = 'rusak';
+
+    const THUMBNAIL_IMAGES = 'THUMBNAIL_IMAGES';
+
     protected $guarded = [];
     protected $fillable = [
         'id',
