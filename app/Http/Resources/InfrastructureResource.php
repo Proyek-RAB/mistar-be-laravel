3dff3881f9fbfbe05f5b4361a8d38c031656b6ea
akramab <m.akram.bari@gmail.com>
2023-11-09 22:37:08 +0900

    update

--- app/Http/Resources/InfrastructureResource.php
+++ app/Http/Resources/InfrastructureResource.php
@@ -24,7 +24,7 @@ class InfrastructureResource extends JsonResource
         }
 
         $details = json_decode(json_decode($this->details));
-        $details->description->contact_person = substr_replace($details->description->contact_person, '*', 0, 5);
+        $details->description->contact_person = substr_replace($details->description->contact_person, '*****', 0, 5);
 
         return [
             'id' => $this->id,
