365512154a8c08aa934c8db521e87acdfeaf76b7
akramab <m.akram.bari@gmail.com>
2023-11-20 22:53:08 +0900

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -72,6 +72,7 @@ class AuthController extends Controller
                 [
                     'success' => false,
                     'message' => 'reset token expired',
+                    'test' => $timeDiff,
                     'data' => null
                 ]
             );
